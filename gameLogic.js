6e43a407265d9984106e6085f420673321a61e2c
RandilT <randiltharusha72@gmail.com>
2023-12-19 19:18:28 +0530

    Refactor winning conditions code

--- gameLogic.js
+++ gameLogic.js
@@ -17,8 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 let count = 0;
 
-console.log(winning_combinations[1]);
-
 handleClick = (event) => {
   const isGameOver = true;
   let current = document.querySelector(".current");
@@ -37,215 +35,232 @@ handleClick = (event) => {
   }
   count++;
   let win = document.querySelector(".win-message");
-  // All PLAYER X WINNING CONDITIONS
-  if (
-    (document.getElementById("s1").innerHTML == "X") &
-    (document.getElementById("s2").innerHTML == "X") &
-    (document.getElementById("s3").innerHTML == "X")
-  ) {
-    // Refresh the page
+  console.log(winning_combinations[1]);
 
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
+  for (let i = 0; i < winning_combinations.length; i++) {
+    if (
+      document.getElementById("s" + winning_combinations[i][0]).innerHTML ==
+        "X" &&
+      document.getElementById("s" + winning_combinations[i][1]).innerHTML ==
+        "X" &&
+      document.getElementById("s" + winning_combinations[i][2]).innerHTML == "X"
+    ) {
+      win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+      for (let i = 1; i <= 9; i++) {
+        document
+          .getElementById("s" + i)
+          .removeEventListener("click", handleClick);
+      }
     }
   }
-  if (
-    (document.getElementById("s4").innerHTML == "X") &
-    (document.getElementById("s5").innerHTML == "X") &
-    (document.getElementById("s6").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s7").innerHTML == "X") &
-    (document.getElementById("s8").innerHTML == "X") &
-    (document.getElementById("s9").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s1").innerHTML == "X") &
-    (document.getElementById("s4").innerHTML == "X") &
-    (document.getElementById("s7").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s2").innerHTML == "X") &
-    (document.getElementById("s5").innerHTML == "X") &
-    (document.getElementById("s8").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s3").innerHTML == "X") &
-    (document.getElementById("s6").innerHTML == "X") &
-    (document.getElementById("s9").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s1").innerHTML == "X") &
-    (document.getElementById("s5").innerHTML == "X") &
-    (document.getElementById("s9").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s3").innerHTML == "X") &
-    (document.getElementById("s5").innerHTML == "X") &
-    (document.getElementById("s7").innerHTML == "X")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  // ALL PLAYER O WINNING CONDITIONS
-  if (
-    (document.getElementById("s1").innerHTML == "O") &
-    (document.getElementById("s2").innerHTML == "O") &
-    (document.getElementById("s3").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s4").innerHTML == "O") &
-    (document.getElementById("s5").innerHTML == "O") &
-    (document.getElementById("s6").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s7").innerHTML == "O") &
-    (document.getElementById("s8").innerHTML == "O") &
-    (document.getElementById("s9").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s1").innerHTML == "O") &
-    (document.getElementById("s4").innerHTML == "O") &
-    (document.getElementById("s7").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s2").innerHTML == "O") &
-    (document.getElementById("s5").innerHTML == "O") &
-    (document.getElementById("s8").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s3").innerHTML == "O") &
-    (document.getElementById("s6").innerHTML == "O") &
-    (document.getElementById("s9").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s1").innerHTML == "O") &
-    (document.getElementById("s5").innerHTML == "O") &
-    (document.getElementById("s9").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
-  if (
-    (document.getElementById("s3").innerHTML == "O") &
-    (document.getElementById("s5").innerHTML == "O") &
-    (document.getElementById("s7").innerHTML == "O")
-  ) {
-    win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
-    for (let i = 1; i <= 9; i++) {
-      document
-        .getElementById("s" + i)
-        .removeEventListener("click", handleClick);
-    }
-    // Refresh the page
-  }
+  // All PLAYER X WINNING CONDITIONS
+  //   if (
+  //     (document.getElementById("s1").innerHTML == "X") &
+  //     (document.getElementById("s2").innerHTML == "X") &
+  //     (document.getElementById("s3").innerHTML == "X")
+  //   ) {
+  //     // Refresh the page
+
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //   }
+  //   if (
+  //     (document.getElementById("s4").innerHTML == "X") &
+  //     (document.getElementById("s5").innerHTML == "X") &
+  //     (document.getElementById("s6").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s7").innerHTML == "X") &
+  //     (document.getElementById("s8").innerHTML == "X") &
+  //     (document.getElementById("s9").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s1").innerHTML == "X") &
+  //     (document.getElementById("s4").innerHTML == "X") &
+  //     (document.getElementById("s7").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s2").innerHTML == "X") &
+  //     (document.getElementById("s5").innerHTML == "X") &
+  //     (document.getElementById("s8").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s3").innerHTML == "X") &
+  //     (document.getElementById("s6").innerHTML == "X") &
+  //     (document.getElementById("s9").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s1").innerHTML == "X") &
+  //     (document.getElementById("s5").innerHTML == "X") &
+  //     (document.getElementById("s9").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s3").innerHTML == "X") &
+  //     (document.getElementById("s5").innerHTML == "X") &
+  //     (document.getElementById("s7").innerHTML == "X")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 1 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   // ALL PLAYER O WINNING CONDITIONS
+  //   if (
+  //     (document.getElementById("s1").innerHTML == "O") &
+  //     (document.getElementById("s2").innerHTML == "O") &
+  //     (document.getElementById("s3").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s4").innerHTML == "O") &
+  //     (document.getElementById("s5").innerHTML == "O") &
+  //     (document.getElementById("s6").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s7").innerHTML == "O") &
+  //     (document.getElementById("s8").innerHTML == "O") &
+  //     (document.getElementById("s9").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s1").innerHTML == "O") &
+  //     (document.getElementById("s4").innerHTML == "O") &
+  //     (document.getElementById("s7").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s2").innerHTML == "O") &
+  //     (document.getElementById("s5").innerHTML == "O") &
+  //     (document.getElementById("s8").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s3").innerHTML == "O") &
+  //     (document.getElementById("s6").innerHTML == "O") &
+  //     (document.getElementById("s9").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s1").innerHTML == "O") &
+  //     (document.getElementById("s5").innerHTML == "O") &
+  //     (document.getElementById("s9").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  //     // Refresh the page
+  //   }
+  //   if (
+  //     (document.getElementById("s3").innerHTML == "O") &
+  //     (document.getElementById("s5").innerHTML == "O") &
+  //     (document.getElementById("s7").innerHTML == "O")
+  //   ) {
+  //     win.innerHTML = "Game Over! <span>Player 2 wins!!!</span>";
+  //     for (let i = 1; i <= 9; i++) {
+  //       document
+  //         .getElementById("s" + i)
+  //         .removeEventListener("click", handleClick);
+  //     }
+  // Refresh the page
 };
